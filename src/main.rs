ce64b50dc85cf264274829bd35262ba3d8d9e292
German <germangolozubow@gmail.com>
2025-03-21 19:39:46 +0300

    Add logger init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use log::LevelFilter;
+
 fn main() {
-    println!("Hello, world!");
+    logger_init();
+}
+
+fn logger_init() {
+    let mut builder = env_logger::builder();
+    // Levels
+    if cfg!(debug_assertions) {
+        // Enable all message levels
+        builder
+            .filter_level(LevelFilter::Trace);
+    } else {
+        // Enable error, warn, info levels
+        // debug and trace disabled
+        builder
+            .filter_level(LevelFilter::Info);
+    }
+    builder.format_timestamp(None);
+    builder.format_target(false);
+    builder.init();
 }
