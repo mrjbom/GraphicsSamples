b40811643f4b37f28c356946fea170b25e8e3d77
German <germangolozubow@gmail.com>
2025-03-21 20:06:01 +0300

    Add window creation code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,53 @@
 use log::LevelFilter;
+use std::sync::Arc;
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+use winit::window::{Window, WindowId};
 
 fn main() {
     logger_init();
+
+    let event_loop = EventLoop::new().expect("Failed to create event loop");
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    let mut app = App::default();
+    event_loop.run_app(&mut app).expect("Failed to run app");
+}
+
+#[derive(Default)]
+struct App {
+    state: Option<State>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let window = Arc::new(
+            event_loop
+                .create_window(Window::default_attributes())
+                .expect("Failed to create window"),
+        );
+
+        self.state = Some(State { window })
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        window_id: WindowId,
+        event: WindowEvent,
+    ) {
+        let state = self.state.as_mut().unwrap();
+        match event {
+            WindowEvent::CloseRequested => event_loop.exit(),
+            WindowEvent::RedrawRequested => {}
+            _ => {}
+        }
+    }
+}
+
+struct State {
+    window: Arc<Window>,
 }
 
 fn logger_init() {
@@ -9,13 +55,11 @@ fn logger_init() {
     // Levels
     if cfg!(debug_assertions) {
         // Enable all message levels
-        builder
-            .filter_level(LevelFilter::Trace);
+        builder.filter_level(LevelFilter::Trace);
     } else {
         // Enable error, warn, info levels
         // debug and trace disabled
-        builder
-            .filter_level(LevelFilter::Info);
+        builder.filter_level(LevelFilter::Info);
     }
     builder.format_timestamp(None);
     builder.format_target(false);
